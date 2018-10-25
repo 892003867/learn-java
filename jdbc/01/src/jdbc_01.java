5082520f30004d0926c1673cf9120397dbe6a330
lib <892003867@qq.com>
2018-10-25 11:28:03 +0800

    整合sql语句

--- jdbc/01/src/jdbc_01.java
+++ jdbc/01/src/jdbc_01.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class jdbc_01 {
     public static void main(String[] args) throws Exception {
         // 1.不推荐的做法
@@ -7,6 +11,23 @@ public class jdbc_01 {
         Class.forName("com.mysql.jdbc.Driver");
 
         // url连接完整
-        String url="jdbc:mysql://192.168.56.1:8888/jdbc";
+        String url = "jdbc:mysql://192.168.56.1:8888/jdbc?useUnicode=true&characterEncoding=utf8";
+        String username = "root";
+        String password = "123456";
+        Connection c = DriverManager.getConnection(url, username, password);
+        /*
+            com.mysql.jdbc.JDBC4Connection@2b05039f
+            com.mysql.jdbc.JDBC4Connection：实现类
+            @2b05039f：内存地址
+        */
+
+        // 3.获取执行sql操作对象
+        Statement s = c.createStatement();
+        System.out.println(s);
+
+        // 4.执行sql语句，获取结果
+        String insert = "insert into jdbc_01_sort(sname,sprice,sdesc) values('工具',7000,'装潢用品') ";
+        int i = s.executeUpdate(insert);
+        System.out.println(i);
     }
 }
